66a4203f835c75a2485e068d701bbcba03477920
Mirhasankhan <mirhasan000034@gmail.com>
2023-05-18 23:37:31 +0600

    single toys loaded

--- src/Components/Pages/AllToys/ToyDetails.jsx
+++ src/Components/Pages/AllToys/ToyDetails.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
+import { useLoaderData } from 'react-router-dom';
 
 const ToyDetails = () => {
+    const singleToy = useLoaderData()
+    console.log(singleToy);
     return (
         <div>
             details toy
--- src/Components/Routes/Routes.jsx
+++ src/Components/Routes/Routes.jsx
@@ -35,7 +35,8 @@ const router = createBrowserRouter([
             },
             {
                 path: '/allToys/:id',
-                element: <ToyDetails></ToyDetails>
+                element: <ToyDetails></ToyDetails>,
+                loader: ({params})=> fetch(`http://localhost:5000/addToy/${params.id}`)
             }
         ]
     }
